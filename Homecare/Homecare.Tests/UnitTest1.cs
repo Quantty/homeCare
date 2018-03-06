26813581f41458274f4058967fb9b39e91fb8245
Roland <rolandas.get@gmail.com>
2018-03-06 14:00:49 +0100

    Base structure , first test, db connection, connection String in webconfig and app config (rename the server name in connection string)

---
+++ Homecare/Homecare.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Homecare.Database;
+using System.Linq;
+
+namespace Homecare.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataClasses1DataContext dataContext = new DataClasses1DataContext();
+            IQueryable<Person> data = from p in dataContext.Persons select p;
+            Assert.IsTrue(data.Count() > 1);
+        }
+    }
+}
